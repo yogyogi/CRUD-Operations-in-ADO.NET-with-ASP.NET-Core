eedc7cf7eabdd22dca0187debb2fb65c98f27321
Yogi <yogeshcs2003@gmail.com>
2021-02-03 20:27:35 +0530

    updated to .NET 5.0

--- CRUDADO/Controllers/HomeController.cs
+++ CRUDADO/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+//using Microsoft.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using CRUDADO.Models;
